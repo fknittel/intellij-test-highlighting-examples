61505b9f0539dda04e5841363f8dad201e3f5d0b
Fabian Knittel <fabian.knittel@lettink.de>
2022-11-07 14:56:13 +0100

    Add example for multiple failures based on assertj softassertions

--- src/test/java/org/example/intellij/MultipleHighlightingTest.java
+++ src/test/java/org/example/intellij/MultipleHighlightingTest.java
@@ -1,6 +1,7 @@
 package org.example.intellij;
 
 import java.util.List;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import org.opentest4j.AssertionFailedError;
 import org.opentest4j.MultipleFailuresError;
@@ -13,6 +14,14 @@ public class MultipleHighlightingTest {
 
         List<AssertionFailedError> failures = List.of(aFailure1, aFailure2);
         throw new MultipleFailuresError(null, failures);
+    }
+
+    @Test
+    void multipleSoftAssertions() {
+        SoftAssertions softly = new SoftAssertions();
+        softly.collectAssertionError(createAFailure());
+        softly.collectAssertionError(createAFailure());
+        softly.assertAll();
     }
 
     private AssertionFailedError createAFailure() {
